047ae383a899cb71af75d35942d12332459a43ba
luis <luiszkm@gmail.com>
2022-11-29 15:33:09 -0300

    add layout

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,13 @@
 import { Routes, Route } from 'react-router-dom'
+import { DefaultLayout } from './layouts/DefaultLayout'
 import { App } from './pages/App'
 
 export function AppRouter() {
   return (
     <Routes>
-      <Route path="/" element={<App />} />
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<App />} />
+      </Route>
     </Routes>
   )
 }
