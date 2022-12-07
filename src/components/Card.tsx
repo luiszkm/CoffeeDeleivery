5e9159f1b19c4cf8d8dbf6ad81b4451567bfa3a7
luis <luiszkm@gmail.com>
2022-12-07 15:20:24 -0300

    add assets

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,10 +1,11 @@
 import { PurchaseButton } from './PurchaseButton'
+import coffee from '../assets/coffee.jpg'
 
 export function Card() {
   return (
-    <div className="w-64 h-80 bg-base-card rounded-tr-[36px] rounded-bl-[36px] rounded-md ">
+    <div className="w-64 h-80 flex items-center justify-between flex-col bg-base-card rounded-tr-[36px] rounded-bl-[36px] rounded-md ">
       <div>
-        <img src="" alt="" />
+        <img src={coffee} alt="" />
         <span className=" px-1 py-2 rounded-[10px] uppercase font-bold bg-yellow-light text-yellow-dark">
           Tradicional
         </span>
